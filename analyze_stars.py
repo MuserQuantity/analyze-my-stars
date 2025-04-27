15032fe54411b0324c7686c2bfc011affa2ab7f6
muserquantity <muserquantity@gmail.com>
2025-04-28 05:47:12 +0800

    更新项目分析脚本，修改项目描述字段为摘要和详细信息，调整分析指令为中文，并更新项目选择逻辑。

--- analyze_stars.py
+++ analyze_stars.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import os
 import openai
-from agents import Agent, OpenAIChatCompletionsModel, RunConfig, Runner
+from agents import Agent, ModelSettings, OpenAIChatCompletionsModel, RunConfig, Runner
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
@@ -16,7 +16,7 @@ model_name = os.getenv("OPENAI_MODEL")
 with open('all_stars.json', 'r', encoding='utf-8') as f:
     projects = json.load(f)
 
-project = projects[0]
+project = projects[1]
 
 # 解析project为key:value的格式的字符串
 # 例如 <full_name> test </full_name> <description> test </description> <language> test </language>
@@ -27,23 +27,32 @@ for key, value in project.items():
 # 使用openai的agents
 class ProjectInfo(BaseModel):
     name: str
-    description: str
+    summary: str
+    detail: str
     url: str
     language: str
     stars: int
     topics: list[str]
     types: list[str]
 
+prompt = f"""
+分析github项目，并返回项目信息，包括项目名称、项目描述、项目语言、项目星数、项目url、项目主题、项目类型。
+需要使用中文分析，必要的情况下，保留一部分英文说明，不要机翻。
+其中，
+topic有：AI、Web、Mobile、Desktop、Server、Game、Other。
+type有：Library、Tool、Framework、Plugin、Service、Other。
+
+"""
 
 agent = Agent(
-    name="Project analyzer",
-    instructions="Analyze the project and return the project info",
+    name="github项目分析",
+    instructions=prompt,
     model=OpenAIChatCompletionsModel( 
         model=model_name,
         openai_client=openai.AsyncOpenAI(
             api_key=api_key,
-            base_url=base_url,
-        )
+            base_url=base_url
+        ),
     ),
     output_type=ProjectInfo,
 )
@@ -54,12 +63,12 @@ async def main():
         result = await Runner.run(
             agent, 
             project_str,
-            run_config=RunConfig(tracing_disabled=True)
-
+            run_config=RunConfig(tracing_disabled=True, model_settings=ModelSettings(temperature=0.2))
         )
         final_output = result.final_output
         print(f"Project: {final_output.name}")
-        print(f"Description: {final_output.description}")
+        print(f"Summary: {final_output.summary}")
+        print(f"Detail: {final_output.detail}")
         print(f"Language: {final_output.language}")
         print(f"Stars: {final_output.stars}")
         print(f"Topics: {final_output.topics}")
